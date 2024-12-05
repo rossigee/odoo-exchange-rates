8a4762bd5d1ef090662c852aaf775b5bf2723745
Ross Golder <ross@golder.org>
2024-12-05 19:39:56 +0700

    Make gauges global variables.

--- exchange_rate_metrics/controllers/prometheus_metrics.py
+++ exchange_rate_metrics/controllers/prometheus_metrics.py
@@ -7,10 +7,10 @@ from odoo.http import Controller, route, request
 
 import time
 
-class PrometheusController(Controller):
-    _g1 = Gauge('exchange_rate', 'Exchange rate compared to main currency', ['name'])
-    _g2 = Gauge('exchange_rate_date', 'Exchange rate last updated', ['name'])
+exchange_rate_g1 = Gauge('exchange_rate', 'Exchange rate compared to main currency', ['name'])
+exchange_rate_g2 = Gauge('exchange_rate_date', 'Exchange rate last updated', ['name'])
 
+class PrometheusController(Controller):
     @route("/metrics", auth='token')
     def metrics(self, **kw):
         currencies = request.env['res.currency'].search([
@@ -19,9 +19,9 @@ class PrometheusController(Controller):
         for c in currencies:
             if not c['date']:
                 continue
-            t = time.mktime(c['date'].timetuple()) 
-            self._g1.labels(c['name']).set(c['rate'])
-            self._g2.labels(c['name']).set(t)
+            t = time.mktime(c['date'].timetuple())
+            exchange_rate_g1.labels(c['name']).set(c['rate'])
+            exchange_rate_g2.labels(c['name']).set(t)
 
         headers = {'Content-Type': 'text/plain'}
         return request.make_response(generate_latest(), headers=headers)
