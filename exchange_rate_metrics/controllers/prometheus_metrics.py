e5888d9f43d6219a3aa319a791a7a5cdd646c083
Ross Golder <ross@golder.org>
2024-12-12 17:45:17 +0700

    Contain metrics gauges in a singleton class.

--- exchange_rate_metrics/controllers/prometheus_metrics.py
+++ exchange_rate_metrics/controllers/prometheus_metrics.py
@@ -7,12 +7,23 @@ from odoo.http import Controller, route, request
 
 import time
 
-exchange_rate_g1 = Gauge('exchange_rate', 'Exchange rate compared to main currency', ['name'])
-exchange_rate_g2 = Gauge('exchange_rate_date', 'Exchange rate last updated', ['name'])
+class Singleton(type):
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+class ExchangeRateMetrics(metaclass=Singleton):
+    def __init__(self):
+        self.g1 = Gauge('exchange_rate', 'Exchange rate compared to main currency', ['name'])
+        self.g2 = Gauge('exchange_rate_date', 'Exchange rate last updated', ['name'])
 
 class PrometheusController(Controller):
     @route("/metrics", auth='token')
     def metrics(self, **kw):
+        m = ExchangeRateMetrics()
+
         currencies = request.env['res.currency'].search([
             ('active', '=', 'true')
         ])
@@ -20,8 +31,8 @@ class PrometheusController(Controller):
             if not c['date']:
                 continue
             t = time.mktime(c['date'].timetuple())
-            exchange_rate_g1.labels(c['name']).set(c['rate'])
-            exchange_rate_g2.labels(c['name']).set(t)
+            m.g1.labels(c['name']).set(c['rate'])
+            m.g2.labels(c['name']).set(t)
 
         headers = {'Content-Type': 'text/plain'}
         return request.make_response(generate_latest(), headers=headers)
